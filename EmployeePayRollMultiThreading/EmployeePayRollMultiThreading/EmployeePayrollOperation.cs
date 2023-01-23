0a5c29a2cf5aee26f675402c2af8ae53016b78d8
qismat <qismatnisha6243@gmail.com>
2023-01-23 22:08:02 +0530

    [qismat] Add :UC1-TimeTakenNoThread

---
+++ EmployeePayRollMultiThreading/EmployeePayRollMultiThreading/EmployeePayrollOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRollMultiThreading
+{
+    public class EmployeePayrollOperation
+    {
+        List<EmployeeDetail> employeeDetails = new List<EmployeeDetail>();
+        public void AddEmployeeToPayroll(List<EmployeeDetail> listemp)
+        {
+            listemp.ForEach(empdata =>
+            {
+                Console.WriteLine("employee being added:" + empdata.EmployeeName);
+                this.AddEmployeeToPayroll(empdata);
+                Console.WriteLine("employee added:" + empdata.EmployeeName);
+            });
+
+        }
+        private void AddEmployeeToPayroll(EmployeeDetail emp)
+        {
+            employeeDetails.Add(emp);
+        }
+    }
+}
---
+++ EmployeePayRollMultiThreading/EmployeePayRollMultiThreading/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayRollMultiThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Multi Threading Problem");
+        }
+    }
+}
